d5deba8d0f70771cad5f9549619abb7e4ad8123a
Aleksey Golovenchik <59099639+AlGol86@users.noreply.github.com>
2021-02-16 23:31:09 +0300

    Update ExampleX.java

--- ExampleX.java
+++ ExampleX.java
@@ -2,7 +2,7 @@ public class ExampleX {
     public static void main(String[] args) {
 
 
-        Person tom = new Person("Tom", "qwerty");
+        Person tom = new Person("Tom", "qwertyyyyyyyyyyyyyyyyyyyy");
         tom.displayPerson();
         tom.account.displayAccount();
     }
